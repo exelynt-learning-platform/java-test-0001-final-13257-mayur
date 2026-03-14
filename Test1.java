20e06361226ec89774c2506f94d605bb497b525f
mayurgaikwad29799-hub <mayurgaikwad29799@gmail.com>
2026-03-14 17:29:58 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,49 @@
+package best;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+
+	       
+        for (int i = 1; i <=5; i++) {
+
+           
+            for (int j = i; j < 5; j++) {
+                System.out.print(" ");
+            }
+
+            
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        
+        for (int i = 5 - 1; i >= 1; i--) {
+
+            
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+
+           
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    `
+
+	}
+
+}
